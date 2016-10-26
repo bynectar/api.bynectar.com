34620cfd9a1044c2d924eb1228b58ca091d7671b
johnthedesigner <john@johnthedesigner.com>
2016-10-26 13:42:25 -0400

    added galleries page and a single gallery view

---
+++ models/Photo.js
@@ -0,0 +1,27 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * Photo Model
+ * =============
+ */
+
+var Photo = new keystone.List('Photo', {
+	map: { name: 'title' },
+	autokey: { from: 'title', path: 'key', unique: true },
+});
+
+Photo.add({
+	title: {
+		type: Types.Text,
+		required: true,
+		value: '',
+		initial: true
+	},
+	publishedDate: { type: Types.Datetime, default: Date.now },
+	image: { type: Types.CloudinaryImage },
+	caption: { type: Types.Text },
+	photographer: { type: Types.Relationship, ref: 'Vendor', filters: { type: 'photographer' } }
+});
+
+Photo.register();
--- routes/views/gallery.js
+++ routes/views/gallery.js
@@ -7,11 +7,39 @@ exports = module.exports = function (req, res) {
 
 	// Set locals
 	locals.section = 'gallery';
+	locals.filters = {
+		gallery: req.params.gallery,
+	};
+	locals.data = {
+		galleries: [],
+	};
 
-	// Load the galleries by sortOrder
-	view.query('galleries', keystone.list('Gallery').model.find().sort('sortOrder'));
+	// Load the current gallery
+	view.on('init', function (next) {
+
+		var q = keystone.list('Gallery').model.findOne({
+			key: locals.filters.gallery,
+		}).populate({path: 'thumbnail gridImages', populate: ['photographer']});
+
+		q.exec(function (err, result) {
+			locals.data.gallery = result;
+			next(err);
+		});
+
+	});
+
+	// Load other galleries
+	view.on('init', function (next) {
+
+		var q = keystone.list('Gallery').model.find().populate('gridImages').limit('4');
+
+		q.exec(function (err, results) {
+			locals.data.galleries = results;
+			next(err);
+		});
+
+	});
 
 	// Render the view
 	view.render('gallery');
-
 };
